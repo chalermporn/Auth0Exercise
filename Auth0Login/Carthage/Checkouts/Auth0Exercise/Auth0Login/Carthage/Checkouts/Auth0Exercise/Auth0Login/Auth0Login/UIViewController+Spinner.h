d7e21b520e8253943709def3b5be40a453ecabce
Weien Wang <weienw@gmail.com>
2016-07-06 17:12:51 -0500

    [ADD] updated carthage files/dir, after successful carthage build --no-skip-current

---
+++ Auth0Login/Carthage/Checkouts/Auth0Exercise/Auth0Login/Carthage/Checkouts/Auth0Exercise/Auth0Login/Auth0Login/UIViewController+Spinner.h
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Spinner.h
+//  Auth0Login
+//
+//  Created by Weien Wang on 7/6/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Spinner)
+- (UIActivityIndicatorView*) startSpinner:(UIActivityIndicatorView*)spinner inView:(UIView*)view;
+- (void) stopSpinner:(UIActivityIndicatorView*)spinner;
+@end
---
+++ Auth0Login/Carthage/Checkouts/Auth0Exercise/Auth0Login/Carthage/Checkouts/Auth0Exercise/Auth0Login/Auth0Login/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Auth0Login
+//
+//  Created by Weien Wang on 7/4/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
