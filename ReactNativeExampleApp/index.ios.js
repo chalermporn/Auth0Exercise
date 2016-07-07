5b5a748412e413fdf6f9d64ec5e69d87a70faf91
Weien Wang <weienw@gmail.com>
2016-07-07 14:04:43 -0500

    [NEW] ReactNativeExampleApp, for using our iOS component

---
+++ ReactNativeExampleApp/index.ios.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+class ReactNativeExampleApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <TouchableHighlight
+    	  style={styles.button}
+          onPress={this.buttonClicked}>
+        <View>
+          <Text style={styles.buttonText}>Please tap me!</Text>
+        </View>
+      </TouchableHighlight>
+      </View>
+    );
+  }
+  buttonClicked(event) {
+    console.log('button clicked');
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeExampleApp', () => ReactNativeExampleApp);
